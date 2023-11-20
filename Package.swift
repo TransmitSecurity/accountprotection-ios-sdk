5e0efa1fe7f10f2fca4b7569627c2060f7b1ea93
Igor Babitski <igor.babitski@transmitsecurity.com>
2023-11-20 10:52:04 +0200

    new SDK version 2.0.5

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["AccountProtection", "AccountProtection-Dependencies"])
     ],
     dependencies: [
-        .package(url: "https://github.com/TransmitSecurity/core-ios-sdk.git", from: "1.0.15")
+        .package(url: "https://github.com/TransmitSecurity/core-ios-sdk.git", from: "1.0.17")
     ],
     targets: [
         .binaryTarget(
